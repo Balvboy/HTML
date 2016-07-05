5890da0e92d42f1808e20daadcc42e8bd7bf0e62
zhou(iMac) <bravemanzhou@126.com>
2016-07-05 18:04:41 +0800

    update
    update  some file

--- demo/js/api.js
+++ demo/js/api.js
@@ -1,15 +1,21 @@
 $(function(){
 	$(".list-group-item").click(function(e){
-		$(".active").removeClass("active");
-		$(e.target).addClass("active");
-		$(".content h2").text($(e.target).text());
+		if($(e.target).attr("id")=="addbtn"){
+			$('#add').modal();
+		}else{
+			$(".active").removeClass("active");
+			$(e.target).addClass("active");
+			$(".content h2").text($(e.target).text());
+		}
 	});
 	$(".version li a").click(function(e){
 		var content = $(e.target).text();
 		var versiondata = $(e.target).attr("version-data");
 		$(".version-drop").html(content+"<span class='caret'></span>");
 		$(".alert-info").text(versiondata);
-		//window.location.reload();
+	});
 
+	$(".list-group-item").dblclick(function(e){
+		$('#update').modal()
 	});
 })
